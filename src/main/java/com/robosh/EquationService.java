695bda4ff551f4028e8f06217c995f4c0ec334fe
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-09-23 19:37:25 +0300

    fix bug

--- src/main/java/com/robosh/EquationService.java
+++ src/main/java/com/robosh/EquationService.java
@@ -13,6 +13,9 @@ public class EquationService {
         double c = equationConstants.getC();
 
         double discriminant = b * b - 4 * a * c;
+        if(a == 0){
+            throw new RuntimeException("Not quadratic equation");
+        }
 
         if (discriminant < 0){
             throw new RuntimeException("No roots in this quadratic equation");
@@ -22,7 +25,7 @@ public class EquationService {
 
     private double[] calculateRoots(double a, double b, double discriminant) {
         if (discriminant == 0) {
-            double x = -b / 2*a;
+            double x = -b / (2*a);
             return new double[]{x};
         }
         double x1 = (-b + Math.sqrt(discriminant))/(2*a);
